8192e27ec96dcd53dd716bad7f0bbbdfca05b5d9
Abdulrahman AlKhaldi <alkhaldirahman@gmail.com>
2024-10-31 19:05:46 +0300

    Project setup

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Abdulrahman AlKhaldi on 31/10/2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
